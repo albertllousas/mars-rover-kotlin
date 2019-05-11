43df461cb43ad0f265af10a68340f36364a820e6
albertortizn26 <albert.ortiz@n26.com>
2019-05-12 00:02:53 +0200

    Refactor cli

--- src/main/kotlin/com/wallapop/marsrover/cli/MarsRover.kt
+++ src/main/kotlin/com/wallapop/marsrover/cli/MarsRover.kt
@@ -14,7 +14,7 @@ import com.github.ajalt.clikt.parameters.options.multiple
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.choice
 import com.wallapop.marsrover.core.model.Direction
-import com.wallapop.marsrover.core.model.Direction.*
+import com.wallapop.marsrover.core.model.Direction.NORTH
 import com.wallapop.marsrover.core.model.Grid
 import com.wallapop.marsrover.core.model.Point
 import com.wallapop.marsrover.core.model.RoverCommand
@@ -25,7 +25,8 @@ class MarsRover(
     private val parsePoint: DomainParser<Point> = Parsers::parsePoint,
     private val parseDirection: DomainParser<Direction> = Parsers::parseDirection,
     private val console: CliktConsole = defaultCliktConsole()
-) : CliktCommand(help = """
+) : CliktCommand(
+    help = """
     This script moves a rover around on a given grid.
 
     COMMANDS:
@@ -33,38 +34,50 @@ class MarsRover(
         - Move rover backward   -> b
         - Turn the rover left   -> l
         - Turn the rover right  -> r
-    """) {
+    """
+) {
 
     companion object {
         const val pointFormatMessage = "format 'x y'"
     }
 
-    val commands by argument(help="array of commands")
-        .convert { parseCommands(it).getOrElse { error ->  fail(error.message?:"invalid commands") } }
-
-    val grid by option("--map-size", help = "map size - format 'x y'")
-        .convert {
-            parseGrid(it).getOrElse { fail(pointFormatMessage) }
-        }.default(Grid(10, 10))
-
-    val initialPoint by option(help = "initial rover starting point - format 'x y'")
-        .convert {
-            parsePoint(it).getOrElse { fail(pointFormatMessage) }
-        }.default(Point(0, 0))
-
-    val direction by option(help = "initial rover direction")
-        .choice("N", "W", "S", "E")
-        .convert {
-            parseDirection(it).getOrDefault { NORTH }
-        }.default(NORTH )
-
-
-    val obstacles by option("--obstacle", help = "initial starting point - format 'x y'")
-        .convert {
-            parsePoint(it).getOrElse { fail(pointFormatMessage) }
-        }.multiple()
+    val commands by commandsArgument()
+    val grid by mapSizeOption()
+    val initialPoint by initialPointOption()
+    val direction by directionOption()
+    val obstacles by obstaclesOption()
 
     override fun run() {
         TermUi.echo("Hello World!", console = console)
     }
+
+    private fun commandsArgument() =
+        argument(help = "array of commands")
+            .convert { parseCommands(it).getOrElse { error -> fail(error.message ?: "invalid commands") } }
+
+    private fun mapSizeOption() =
+        option("--map-size", help = "map size - format 'x y'")
+            .convert {
+                parseGrid(it).getOrElse { fail(pointFormatMessage) }
+            }.default(Grid(10, 10))
+
+    private fun initialPointOption() =
+        option(help = "initial rover starting point - format 'x y'")
+            .convert {
+                parsePoint(it).getOrElse { fail(pointFormatMessage) }
+            }.default(Point(0, 0))
+
+    private fun directionOption() =
+        option(help = "initial rover direction")
+            .choice("N", "W", "S", "E")
+            .convert {
+                parseDirection(it).getOrDefault { NORTH }
+            }.default(NORTH)
+
+    private fun obstaclesOption() =
+        option("--obstacle", help = "initial starting point - format 'x y'")
+            .convert {
+                parsePoint(it).getOrElse { fail(pointFormatMessage) }
+            }.multiple()
+
 }
