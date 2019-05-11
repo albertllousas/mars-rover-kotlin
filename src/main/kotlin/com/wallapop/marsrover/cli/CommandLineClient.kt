e8418a3ea330a8a777d3d4445c4a99bfc8ca17b4
albertortizn26 <albert.ortiz@n26.com>
2019-05-11 19:03:24 +0200

    Add initial direction option to command line client

--- src/main/kotlin/com/wallapop/marsrover/cli/CommandLineClient.kt
+++ src/main/kotlin/com/wallapop/marsrover/cli/CommandLineClient.kt
@@ -1,5 +1,6 @@
 package com.wallapop.marsrover.cli
 
+import arrow.core.getOrDefault
 import arrow.core.getOrElse
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.output.CliktConsole
@@ -9,13 +10,15 @@ import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.multiple
 import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.parameters.types.choice
+import com.wallapop.marsrover.core.model.Direction
 import com.wallapop.marsrover.core.model.Grid
 import com.wallapop.marsrover.core.model.Point
 
 class CommandLineClient(
     private val parseGrid: DomainParser<Grid> = Parsers::parseGrid,
     private val parsePoint: DomainParser<Point> = Parsers::parsePoint,
+    private val parseDirection: DomainParser<Direction> = Parsers::parseDirection,
     private val console: CliktConsole = defaultCliktConsole()
 ) : CliktCommand() {
 
@@ -24,10 +27,17 @@ class CommandLineClient(
             parseGrid(it).getOrElse { fail("format 'x y'") }
         }.default(Grid(10, 10))
 
-    val initialPoint by option(help = "initial starting point - format 'x y'")
+    val initialPoint by option(help = "initial rover starting point - format 'x y'")
         .convert {
             parsePoint(it).getOrElse { fail("format 'x y'") }
         }.default(Point(0, 0))
+
+    val direction by option(help = "initial rover direction - ")
+        .choice("N", "W", "S", "E")
+        .convert {
+            parseDirection(it).getOrDefault { Direction.NORTH }
+        }
+
 
     val obstacles by option("--obstacle", help = "initial starting point - format 'x y'")
         .convert {
--- src/test/kotlin/com/wallapop/marsrover/cli/CommandLineClientTest.kt
+++ src/test/kotlin/com/wallapop/marsrover/cli/CommandLineClientTest.kt
@@ -9,6 +9,7 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.wallapop.marsrover.core.model.Direction.NORTH
 import com.wallapop.marsrover.core.model.Grid
 import com.wallapop.marsrover.core.model.Point
 import org.junit.jupiter.api.Test
@@ -27,18 +28,15 @@ internal class CommandLineClientTest {
 
     @Test
     fun `command line client accepts map size option`() {
-
         given(gridParser.invoke(any())).willReturn(Try.just(Grid(1, 2)))
 
         commandLineClient.main(arrayOf("--map-size=1 2"))
 
         assertThat(commandLineClient.grid).isEqualTo(Grid(1, 2))
-
     }
 
     @Test
     fun `command line client accepts multiple obstacles options`() {
-
         given(pointParser.invoke(any()))
             .willReturn(Try.just(Point(1, 2)))
             .willReturn(Try.just(Point(2, 3)))
@@ -46,23 +44,28 @@ internal class CommandLineClientTest {
         commandLineClient.main(arrayOf("--obstacle=1 2", "--obstacle=2 3"))
 
         assertThat(commandLineClient.obstacles).isEqualTo(listOf(Point(1, 2), Point(2, 3)))
-
     }
 
     @Test
-    fun `command line client accepts initial starting option`() {
-
+    fun `command line client accepts initial rover starting option`() {
         given(pointParser.invoke(any())).willReturn(Try.just(Point(0, 0)))
 
         commandLineClient.main(arrayOf("--initial-point=0 0"))
 
         assertThat(commandLineClient.initialPoint).isEqualTo(Point(0, 0))
+    }
 
+    @Test
+    fun `command line client accepts initial rover direction`() {
+        given(pointParser.invoke(any())).willReturn(Try.just(Point(0, 0)))
+
+        commandLineClient.main(arrayOf("--direction=N"))
+
+        assertThat(commandLineClient.direction).isEqualTo(NORTH)
     }
 
     @Test
     fun `command line client prints hello world`() {
-
         given(gridParser.invoke(any())).willReturn(Try.just(Grid(1, 2)))
 
         commandLineClient.main(emptyArray())
@@ -71,8 +74,8 @@ internal class CommandLineClientTest {
             assertThat(commandLineClient.initialPoint).isEqualTo(Point(0, 0))
             assertThat(commandLineClient.grid).isEqualTo(Grid(10, 10))
             assertThat(commandLineClient.obstacles).isEqualTo(emptyList<Point>())
+            assertThat(commandLineClient.direction).isEqualTo(NORTH)
         }
-
         verify(console).print("Hello World!\n", false)
     }
 
