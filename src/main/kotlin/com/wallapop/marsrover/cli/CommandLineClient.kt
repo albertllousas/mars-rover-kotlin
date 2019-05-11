74b2f00dff4d4af89f8970c5a46a7fbca65de649
albertortizn26 <albert.ortiz@n26.com>
2019-05-11 17:14:10 +0200

    Add map size option to command line client

--- src/main/kotlin/com/wallapop/marsrover/cli/CommandLineClient.kt
+++ src/main/kotlin/com/wallapop/marsrover/cli/CommandLineClient.kt
@@ -1,11 +1,24 @@
 package com.wallapop.marsrover.cli
 
+import arrow.core.getOrElse
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.output.CliktConsole
 import com.github.ajalt.clikt.output.TermUi
 import com.github.ajalt.clikt.output.defaultCliktConsole
+import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
+import com.wallapop.marsrover.core.model.Grid
 
-class CommandLineClient(private val console: CliktConsole = defaultCliktConsole()) : CliktCommand() {
+class CommandLineClient(
+    private val parseGrid: DomainParser<Grid> = Parsers::parseGrid,
+    private val console: CliktConsole = defaultCliktConsole()
+) : CliktCommand() {
+
+    val grid by option("--map-size", help = "map size - format 'x y'")
+        .convert {
+            parseGrid(it).getOrElse { fail("format 'x y'") }
+        }.prompt("Insert map size (x,y)")
 
     override fun run() {
         TermUi.echo("Hello World!", console = console)
--- src/test/kotlin/com/wallapop/marsrover/cli/CommandLineClientTest.kt
+++ src/test/kotlin/com/wallapop/marsrover/cli/CommandLineClientTest.kt
@@ -1,9 +1,14 @@
 package com.wallapop.marsrover.cli
 
+import arrow.core.Try
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import com.github.ajalt.clikt.output.CliktConsole
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.wallapop.marsrover.core.model.Grid
 import org.junit.jupiter.api.Test
 
 internal class CommandLineClientTest {
@@ -12,10 +17,32 @@ internal class CommandLineClientTest {
         given(it.lineSeparator).willReturn("\n")
     }
 
+    private val gridParser = mock<DomainParser<Grid>>()
+
+    private val commandLineClient = CommandLineClient(console = console)
+
+    private val arguments = arrayOf("--map-size=1 2")
+
+    @Test
+    fun `command line client accepts map size option`() {
+
+        given(gridParser.invoke(any())).willReturn(Try.just(Grid(1, 2)))
+
+        commandLineClient.main(arguments)
+
+        assertThat(commandLineClient.grid).isEqualTo(Grid(1, 2))
+
+    }
+
     @Test
     fun `command line client prints hello world`() {
-        CommandLineClient(console).run()
+
+        given(gridParser.invoke(any())).willReturn(Try.just(Grid(1, 2)))
+
+        commandLineClient.main(arguments)
 
         verify(console).print("Hello World!\n", false)
     }
+
+
 }
