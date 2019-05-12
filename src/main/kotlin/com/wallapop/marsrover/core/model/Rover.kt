365d0f57ed1c693b066ed951c437700b88eb6939
albertortizn26 <albert.ortiz@n26.com>
2019-05-12 07:02:56 +0200

    Move rover when sequence of commands

--- src/main/kotlin/com/wallapop/marsrover/core/model/Rover.kt
+++ src/main/kotlin/com/wallapop/marsrover/core/model/Rover.kt
@@ -1,16 +1,25 @@
 package com.wallapop.marsrover.core.model
 
 import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.Either.Right
 
-private val noop = { _: Any -> Unit }
 
 data class Rover(
     val position: Position,
-    val move: (Position, RoverCommand) -> Either<Obstacle, Position>,
-    val report: (Obstacle) -> Unit = noop
+    val move: (Position, RoverCommand) -> Either<Obstacle, Position>
 ) {
     fun execute(commands: List<RoverCommand>): Position {
-        return position
+        return execute(position, commands)
     }
 
+    private tailrec fun execute(currentPosition: Position, commands: List<RoverCommand>): Position =
+        if (commands.isEmpty()) {
+            currentPosition
+        } else {
+            when (val movement = move(currentPosition, commands.first())) {
+                is Left -> currentPosition
+                is Right -> execute(movement.b, commands.drop(1))
+            }
+        }
 }
--- src/test/kotlin/com/wallapop/marsrover/core/model/RoverTest.kt
+++ src/test/kotlin/com/wallapop/marsrover/core/model/RoverTest.kt
@@ -1,14 +1,13 @@
 package com.wallapop.marsrover.core.model
 
 import arrow.core.Either
+import arrow.core.Right
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
-import com.wallapop.marsrover.core.model.Direction.*
+import com.wallapop.marsrover.core.model.Direction.EAST
+import com.wallapop.marsrover.core.model.Direction.NORTH
 import org.junit.jupiter.api.Test
 
 internal class RoverTest {
@@ -23,5 +22,22 @@ internal class RoverTest {
         val actual = rover.execute(emptyList())
 
         assertThat(actual).isEqualTo(initialPosition)
+    }
+
+    @Test
+    fun `should move rover for and array of commands`() {
+        val initialPosition = Position(Point(1, 1), NORTH)
+        val secondPosition = Position(Point(1, 2), NORTH)
+        val thirdPosition = Position(Point(1, 2), EAST)
+        val finalPosition = Position(Point(2, 2), EAST)
+        val rover = Rover(initialPosition, move)
+        val commands = listOf(MoveForward, TurnRight, MoveForward)
+        given(move.invoke(initialPosition, MoveForward)).willReturn(Right(secondPosition))
+        given(move.invoke(secondPosition, TurnRight)).willReturn(Right(thirdPosition))
+        given(move.invoke(thirdPosition, MoveForward)).willReturn(Right(finalPosition))
+
+        val actual = rover.execute(commands)
+
+        assertThat(actual).isEqualTo(finalPosition)
     }
 }
