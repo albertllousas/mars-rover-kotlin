ecadad45d8c4b5f5d0545227f23c997a1d470e92
albertortizn26 <albert.ortiz@n26.com>
2019-05-12 15:19:45 +0200

    Add move to the different directions as a method in Point model

--- src/main/kotlin/com/wallapop/marsrover/core/model/Point.kt
+++ src/main/kotlin/com/wallapop/marsrover/core/model/Point.kt
@@ -1,3 +1,11 @@
 package com.wallapop.marsrover.core.model
 
-data class Point(val x:Int, val y:Int)
+data class Point(val x: Int, val y: Int) {
+    fun moveTo(direction: Direction) =
+        when (direction) {
+            Direction.NORTH -> this.copy(y = y.inc())
+            Direction.SOUTH -> this.copy(y = y.dec())
+            Direction.EAST -> this.copy(x = x.inc())
+            Direction.WEST -> this.copy(x = x.dec())
+        }
+}
--- src/test/kotlin/com/wallapop/marsrover/core/model/PointTest.kt
+++ src/test/kotlin/com/wallapop/marsrover/core/model/PointTest.kt
@@ -1,5 +1,31 @@
 package com.wallapop.marsrover.core.model
 
-import org.junit.jupiter.api.Assertions.*
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.wallapop.marsrover.core.model.Direction.*
+import org.junit.Test
 
-internal class PointTest
+internal class PointTest {
+    
+    private val point = Point(2, 2)
+    
+    @Test
+    fun `should move north`() {
+        assertThat(point.moveTo(NORTH)).isEqualTo(Point(2, 3))
+    }
+
+    @Test
+    fun `should move south`() {
+        assertThat(point.moveTo(SOUTH)).isEqualTo(Point(2, 1))
+    }
+
+    @Test
+    fun `should move east`() {
+        assertThat(point.moveTo(EAST)).isEqualTo(Point(3, 2))
+    }
+
+    @Test
+    fun `should move west`() {
+        assertThat(point.moveTo(WEST)).isEqualTo(Point(1, 2))
+    }
+}
